f63c31cdd4147a2aad73cd88bd79c39240e4f405
Herbert Gomez <herbertgoto@192.168.0.102>
2018-01-17 19:48:12 -0500

    Query method and tests

--- src/main/java/com/cube/demo/CubeSumation.java
+++ src/main/java/com/cube/demo/CubeSumation.java
@@ -1,6 +1,7 @@
 package com.cube.demo;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -8,8 +9,10 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
@@ -18,6 +21,7 @@ import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -40,16 +44,25 @@ public class CubeSumation implements RequestHandler<Object, String> {
 
 		//if (input.equals("CreateMatrix")) {
 			createMatrix(matrixName);
-			populateMatrix(2, matrixName);
+			populateMatrix(4, matrixName);
 
 			Table table = dynamoDB.getTable(matrixName);
 			//return "" + table.describe().getItemCount();
 		//} else if(input.equals("UploadItem")) {
-			updateItem(111, 4, matrixName);
+			updateItem(222, 4, matrixName);
+			queryMatrix(111, 333, matrixName);	
+			updateItem(111, 23, matrixName);
+			queryMatrix(222, 444, matrixName);
+			queryMatrix(111, 333, matrixName);
+		//	updateItem(221, 3, matrixName);
+		
+		//	queryMatrix(111, 222, matrixName);	// 9
+		//	queryMatrix(111, 122, matrixName);	// 4
+		//	queryMatrix(211, 222, matrixName);	// 5
 			
-			//System.out.println(table.getItem("pos", 111).get("val"));
+			System.out.println(table.getItem("id", 1,"pos", 112).get("val"));
 			
-			return "" + table.getItem("pos", 111).get("val");
+			return "" + table.getItem("id", 1,"pos", 112).get("val");
 		//}
 		// TODO: implement your handler
 		//return "success";
@@ -65,9 +78,11 @@ public class CubeSumation implements RequestHandler<Object, String> {
 
 		try {
 			System.out.println("Attempting to create matrix structure; please wait...");
-			Table table = dynamoDB.createTable(matrixName, Arrays.asList(new KeySchemaElement("pos", KeyType.HASH)), // Partition
-																														// key
-					Arrays.asList(new AttributeDefinition("pos", ScalarAttributeType.N)),
+			Table table = dynamoDB.createTable(matrixName, Arrays.asList(new KeySchemaElement("id", KeyType.HASH), // Partition key
+					 new KeySchemaElement("pos", KeyType.RANGE)), // Sort key
+																														
+					Arrays.asList(new AttributeDefinition("id", ScalarAttributeType.N),
+							new AttributeDefinition("pos", ScalarAttributeType.N)),
 					new ProvisionedThroughput(10L, 10L));
 			table.waitForActive();
 			System.out.println("Success.  Matrix structure status: " + table.getDescription().getTableStatus());
@@ -89,6 +104,8 @@ public class CubeSumation implements RequestHandler<Object, String> {
 
 		Table table = dynamoDB.getTable(matrixName);
 
+		//int id = 1;
+		
 		for (int i = 1; i < n + 1; i++) {
 			for (int j = 1; j < n + 1; j++) {
 				for (int k = 1; k < n + 1; k++) {
@@ -97,9 +114,10 @@ public class CubeSumation implements RequestHandler<Object, String> {
 
 						int index = Integer.valueOf(String.valueOf(i) + String.valueOf(j) + String.valueOf(k));
 
-						table.putItem(new Item().withPrimaryKey("pos", index).withNumber("val", 0));
+						table.putItem(new Item().withPrimaryKey("id", 1, "pos", index).withNumber("val", 0));
 						// System.out.println("PutItem succeeded: " + index + " " + 0);
-
+						//id++;
+						
 					} catch (Exception e) {
 						System.err.println("Unable to add item!");
 						System.err.println(e.getMessage());
@@ -112,7 +130,7 @@ public class CubeSumation implements RequestHandler<Object, String> {
 	}
 
 	// Upload item
-	private void updateItem(int pos, int val, String matrixName) {
+	private void updateItem(int index, int val, String matrixName) {
 
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
 				new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1")).build();
@@ -121,7 +139,7 @@ public class CubeSumation implements RequestHandler<Object, String> {
 
 		Table table = dynamoDB.getTable(matrixName);
 
-		UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("pos", pos)
+		UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("id", 1, "pos", index)
 				.withUpdateExpression("set val = :v")
 				.withValueMap(new ValueMap().withNumber(":v", val))
 				.withReturnValues(ReturnValue.UPDATED_NEW);
@@ -129,12 +147,57 @@ public class CubeSumation implements RequestHandler<Object, String> {
 		try {
 			//System.out.println("Updating the item...");
 			UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
-			//System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+		//	System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
 
 		} catch (Exception e) {
 			System.err.println("Unable to update item!");
 			System.err.println(e.getMessage());
 		}
+	}
+	
+	// Query matrix
+	private void queryMatrix(int initIndex, int endIndex, String matrixName) {
+		
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+				new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1")).build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+
+		Table table = dynamoDB.getTable(matrixName);
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("id = :v and pos between :pos1 and :pos2")
+        		.withValueMap(new ValueMap()
+        				.withInt(":v", 1)
+        		        .withInt(":pos1", initIndex)
+        		        .withInt(":pos2", endIndex));
+
+        ItemCollection<QueryOutcome> items = null;
+        Iterator<Item> iterator = null;
+        Item item = null;
+        
+        int sum = 0;
+        
+        try {
+            
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                
+                sum = sum + item.getNumber("val").intValue();
+                
+               
+            }
+            
+            System.out.println(sum);
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query items!");
+            System.err.println(e.getMessage());
+        }
+		
 	}
 
 }
